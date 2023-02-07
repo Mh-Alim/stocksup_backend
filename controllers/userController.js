c45f94146818957e34032fe9002b883140de5e72
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2023-02-07 22:14:34 +0530

    entry page backend added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,42 @@
-// const User = require("../models/userModel");
+import Code from "../models/codeModel.js";
+
+// Login User
+const loginUser = async (req, res, next) => {
+  const { code, name } = req.body;
+  console.log("login ", code, name);
+
+  // checking if the user has name and password both
+
+  if (!code || !name) {
+    return res.status(400).json({
+      message: "Fill the empty fields",
+    });
+  }
+
+  const user = await Code.findOne({ code });
+  console.log(user);
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid Code",
+    });
+  }
+
+  user.name = name;
+  user.isActive = true;
+  await user.save();
+  const token = await user.getJWTToken();
+
+  res.status(200).json({
+    success: true,
+    info: user,
+    token,
+  });
+};
+
+const alredyLoggedIn = async (req, res) => {
+  res.status(200).json({
+    success: true,
+  });
+};
+export { loginUser, alredyLoggedIn };
