c9b1a32cdbdb27697352ac9f04f38b939b51c056
jayprakash02 <unijay12@gmail.com>
2023-03-08 19:54:02 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -7,48 +7,11 @@ import http from "http";
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 
-import Portfolio from "./models/portfolio.js";
-import Code from "./models/codeModel.js";
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-// ROUTE IMPORTS
-
-// ERROR MIDDLEWARE -> customErrorHandler
-// const PortfolioRoutes = require("./routes/portfolio");
 import PortfolioRoutes from "./routes/portfolio.js";
 import codeRoutes from "./routes/codeRoutes.js";
 
-// const codeRoutes = require("./routes/codeRoutes.js");
-
-app.use("/portfolios", PortfolioRoutes);
-app.use("/api/v1", codeRoutes);
-// app.use("/api/v1", codeRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-// lakshman08  b6WipDqxkcIsWIkr
-
-mongoose
-  .connect(
-    "mongodb+srv://lakshman08:b6WipDqxkcIsWIkr@cluster0.x5iwwjx.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  // .then(() => app.listen(PORT, () => console.log("listening on port " + PORT)))
-  .then(() => console.log("Mongodb connected successfully"))
-  .catch((issues) => console.log("issues " + issues));
-
-// socket
-
+const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
     origin: [
@@ -62,35 +25,62 @@ const io = new Server(server, {
   },
 });
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/portfolios", PortfolioRoutes);
+app.use("/api/v1", codeRoutes);
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://lakshman08:b6WipDqxkcIsWIkr@cluster0.x5iwwjx.mongodb.net/?retryWrites=true&w=majority";
+
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongodb connected successfully");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error);
+  }
+})();
+
 io.on("connection", (socket) => {
   console.log("socket is connected");
 
-  // at the starting everone will see this getstock
   socket.on("getStock", async (id, userId, cb) => {
-    const totStock = async () => {
+    try {
       const portfolio = await Portfolio.findById(id);
       const user = await Code.findById(userId);
-      console.log(portfolio);
-      return [portfolio.stock, user.userStock];
-    };
-    let a = await totStock();
-
-    cb(a);
+      const totStock = [portfolio.stock, user.userStock];
+      cb(totStock);
+    } catch (error) {
+      console.log("Error getting stock", error);
+    }
   });
 
   socket.on("buy", async (id, userId, buyProd) => {
     let flag = false;
-    const totStock = async () => {
+    try {
       const portfolio = await Portfolio.findById(id);
       const user = await Code.findById(userId);
 
       if (user.userStock < buyProd) {
         socket.emit("userStock-empty");
-        return [portfolio.stock, user.userStock];
+        const totStock = [portfolio.stock, user.userStock];
+        io.to(id).emit("show-stock", totStock);
+        socket.emit("show-userStock", totStock);
+        return;
       }
+
       if (portfolio.stock < buyProd) {
         socket.emit("stock-empty");
-        return [portfolio.stock, user.userStock];
+        const totStock = [portfolio.stock, user.userStock];
+        io.to(id).emit("show-stock", totStock);
+        socket.emit("show-userStock", totStock);
+        return;
       }
 
       flag = true;
@@ -98,12 +88,16 @@ io.on("connection", (socket) => {
       user.userStock -= buyProd;
       await user.save();
       await portfolio.save();
-      return [portfolio.stock, user.userStock];
-    };
-    let remainingStock = await totStock();
-    if (flag) socket.emit("successfully-purchased", buyProd);
-    io.to(id).emit("show-stock", remainingStock);
-    socket.emit("show-userStock", remainingStock);
+      const remainingStock = [portfolio.stock, user.userStock];
+      io.to(id).emit("show-stock", remainingStock);
+      socket.emit("show-userStock", remainingStock);
+    } catch (error) {
+      console.log("Error buying stock", error);
+    }
+
+    if (flag) {
+      socket.emit("successfully-purchased", buyProd);
+    }
   });
 
   socket.on("join-room", (room) => {
