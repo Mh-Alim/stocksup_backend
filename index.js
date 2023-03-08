a10afb1252177eea27742ca84043e5ae7a630a28
jayprakash02 <unijay12@gmail.com>
2023-03-08 19:58:25 +0530

    port updated

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors());
 app.use("/portfolios", PortfolioRoutes);
 app.use("/api/v1", codeRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://lakshman08:b6WipDqxkcIsWIkr@cluster0.x5iwwjx.mongodb.net/?retryWrites=true&w=majority";
 
 (async () => {
